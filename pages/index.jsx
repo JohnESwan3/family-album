35e798d0aadb4c47aa801b8265bb77b8886935c3
John Swan <johneswan3@protonmail.com>
2022-04-27 18:19:44 -0600

    Faker.js | Stories Section | Setup

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
+import Feed from '../components/Feed'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="h-screen overflow-y-scroll bg-slate-50 scrollbar-hide">
       <Head>
         <title>Family Album</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Family Album</h1>
+      <Header />
+      <Feed />
+      {/* Modal */}
     </div>
   )
 }
